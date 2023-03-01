438ee963677c31b7f24f564cd95f3587df2c0dd4
Aixa Filsinger <macafil99@gmail.com>
2023-03-01 03:47:11 -0300

    se realizo el ejercicio 9

---
+++ js/ejercicio9.js
@@ -0,0 +1,11 @@
+let frase = prompt("Ingrese una frase");
+
+
+for( let i= 0; i<=frase.length; i++)
+{
+    if(frase.charAt(i) ==="a" || frase.charAt(i) ==="e" || frase.charAt(i) ==="i" || frase.charAt(i) ==="o" || frase.charAt(i) ==="u")
+    {
+        document.write(`<h1>${frase.charAt(i)}</h1>`);
+    }  
+
+}
