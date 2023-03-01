4031cbb3e8197de59f2aa25f53e5929a17f7192e
Aixa Filsinger <macafil99@gmail.com>
2023-03-01 03:44:38 -0300

    se realizo el ejercicio 6

---
+++ js/ejercicio6.js
@@ -0,0 +1,11 @@
+let num1 = parseInt(prompt("Ingrese un número"));
+
+  let num2 = parseInt(prompt("Ingrese otro número"));
+
+  if(num1 > num2){
+    document.write(`El número ${num1} es el mayor`);
+  }
+  else{
+    document.write(`El número ${num2} es el mayor`);
+
+  }
